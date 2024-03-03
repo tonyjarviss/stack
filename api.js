aaa31794c9a8c7400aaf051577a5779f103400b9
tonyjarviss <157190551+tonyjarviss@users.noreply.github.com>
2024-03-03 15:29:18 +0530

    Update api.js

--- api.js
+++ api.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 async function getDetails(id) {
     try {
         const response = await axios.get(
-            `https://6229f56a-ea18-4a07-b514-e8d8d32b7b36-00-15pr5d1nprwqy.sisko.replit.dev/terabox?url=${id}`
+            `https://79d752ff-1e8f-456f-8fc4-3d72083c6d9d-00-36xkoo8dnwxoq.sisko.replit.dev/terabox?url=${id}`
         );
         return response.data;
     } catch (error) {

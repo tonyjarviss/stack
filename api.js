d9c0d164b714f3f2ecc3001051837b742b7d858c
tonyjarviss <157190551+tonyjarviss@users.noreply.github.com>
2024-03-03 17:53:52 +0530

    Update api.js

--- api.js
+++ api.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 async function getDetails(id) {
     try {
         const response = await axios.get(
-            `https://api-2omy.onrender.com/get?url=${id}`
+            `https://api-2omy.onrender.com/terabox?url=${id}`
         );
         return response.data;
     } catch (error) {

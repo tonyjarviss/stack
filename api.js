42dd53d244e77d97672ebe5b3592e009a052e718
tonyjarviss <157190551+tonyjarviss@users.noreply.github.com>
2024-03-03 15:23:45 +0530

    Update api.js

--- api.js
+++ api.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 async function getDetails(id) {
     try {
         const response = await axios.get(
-            `https://shy-puce-xerus-ring.cyclic.app/tera?url=${id}`
+            `https://6229f56a-ea18-4a07-b514-e8d8d32b7b36-00-15pr5d1nprwqy.sisko.replit.dev/terabox?url=${id}`
         );
         return response.data;
     } catch (error) {

482984b21486feeadff2a3a8429c8c89fe8fb3e0
tonyjarviss <157190551+tonyjarviss@users.noreply.github.com>
2024-03-12 23:38:40 +0530

    Update api.js

--- api.js
+++ api.js
@@ -3,9 +3,9 @@ const axios = require("axios");
 async function getDetails(id) {
     try {
         const response = await axios.get(
-            `https://terabox-app.vercel.app/api?data=${id}`
+            `https://bot-nine-rho.vercel.app/api?data=${id}`
         );
-        return response.data;
+        return response.data;                         
     } catch (error) {
         console.error(error);
     }
